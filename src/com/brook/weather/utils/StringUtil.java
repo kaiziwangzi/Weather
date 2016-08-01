dbcbff50641c9a374ae1632f283dbec0b44bfacb
superyxw <superyxw@163.com>
2016-08-01 13:19:25 +0800

    fix

--- src/com/brook/weather/utils/StringUtil.java
+++ src/com/brook/weather/utils/StringUtil.java
@@ -9,7 +9,7 @@ public class StringUtil {
 		}
 		String body = "<v:Envelope xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns:d='http://www.w3.org/2001/XMLSchema' xmlns:c='http://schemas.xmlsoap.org/soap/encoding/' xmlns:v='http://schemas.xmlsoap.org/soap/envelope/'><v:Header /><v:Body><n0:" +
 				method +" id='o0' c:root='1' xmlns:n0='http://find/'>" +
-				"<name1 i:type='d:string'>rain_tqyb_short_024</name1><name2 i:type='d:string'>20160801</name2>" +
+				name +
 				"</n0:" +method +"></v:Body></v:Envelope>";
 		return body;
 	}
