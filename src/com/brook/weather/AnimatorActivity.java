1771d5b6cb6520fbda26c45d28b2a85e07fa8a5d
superyxw <superyxw@163.com>
2016-08-22 09:21:19 +0800

    add new activity

---
+++ src/com/brook/weather/AnimatorActivity.java
@@ -0,0 +1,30 @@
+package com.brook.weather;
+/**
+ * 动画播放界面
+ * @ClassName: AnimatorActivity 
+ * @Description: TODO
+ * @author yuanxw
+ * @date 2016-8-22 上午9:19:48 
+ * @copyright XLSTUDIO
+ */
+public class AnimatorActivity extends BaseActivity{
+
+	@Override
+	protected void setUpContentView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setUpData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setUpView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
